0e4a69cb0aa19450f309647dcba212b627df9e9e
mateocontreras122 <mcontreras2773@sdsu.edu>
2025-11-03 21:09:56 -0800

    Finished implementing upheap and downheap in heap.h

--- heap.h
+++ heap.h
@@ -31,7 +31,7 @@ struct MinHeap {
             return -1;
         }
 
-        int rootIndex = data[0];
+        int rootIdx = data[0];
         // Replace root with last element
         data[0] = data[size - 1];
         // Remove one from size of heap
@@ -39,17 +39,45 @@ struct MinHeap {
         // Call downheap to restore order
         downheap(0, weightArr);
 
-        return rootIndex;
+        return rootIdx;
     }
 
     // Swap child upward while smaller than parent
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        // While the position in heap is not at the root (below root)
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            // Swap upward if child is smaller than its parent
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            } else { // If not, then done
+                break;
+            }
+        }
     }
 
-    // Move an element down until heap property is restored
+    // Swap parent downward while larger than any child
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            // Find the smallest between parent and its children
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]])
+                smallest = left;
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]])
+                smallest = right;
+
+            // If parent isn't smallest, swap and continue
+            if (smallest != pos) {
+                swap(data[pos], data[smallest]);
+                pos = smallest;
+            } else {
+                break;
+            }
+        }
     }
 };
 
